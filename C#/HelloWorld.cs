88ea39e99d696c6c22ef1a9d0e468742bfb3f1d0
Dark knight FG <148987244+darknightdc@users.noreply.github.com>
2026-04-20 22:08:16 -0300

    Update HelloWorld.cs

--- C#/HelloWorld.cs
+++ C#/HelloWorld.cs
@@ -15,14 +15,12 @@ class Program
 "  #    #  ##  ##  #  # ##  ##       ##   ##  ##  ##  #   # ##  ##        \n" +
 "  #    #   ####   #  #  ####        ##   ##   ####   #   #  ### #  #     \n\n";
 
-        // Cores normais
         for (int i = 0; i < 7; i++)
         {
             Console.ForegroundColor = (ConsoleColor)i;
             Console.Write(A);
         }
 
-        // Cores "bright" (equivalente aproximado)
         for (int i = 8; i < 15; i++)
         {
             Console.ForegroundColor = (ConsoleColor)i;
